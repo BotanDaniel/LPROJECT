a84aa21be5c92e84b50f54f8f068abf7ae1166aa
BotanDaniel <125061968+BotanDaniel@users.noreply.github.com>
2024-02-26 19:07:21 +0200

    Add files via upload

--- LPROJECT/script.js
+++ LPROJECT/script.js
@@ -1,14 +1,19 @@
-"use strict";
-
-//pressing yes script
 document.addEventListener("DOMContentLoaded", function () {
   const yesButton = document.querySelector(".yes-btn");
-  const gifImage = document.getElementById("gif");
-  //changing gif and fixing gif size
+  const noButton = document.querySelector(".no-btn");
+
+  noButton.addEventListener("click", function (event) {
+    yesButton.style.width = "100px";
+    yesButton.style.height = "100px";
+
+    console.log("button pressed");
+  });
+
+  // Changing gif and fixing gif size
   yesButton.addEventListener("click", function () {
+    const gifImage = document.getElementById("gif");
     gifImage.src = "GIFS/second-gif.gif";
     gifImage.width = 200;
-    gifImager.height = 200;
+    gifImage.height = 200;
   });
 });
-//pressing no script
